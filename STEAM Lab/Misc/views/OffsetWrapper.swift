249ebfa9bf587db53d57cb8cafba51cc2d25d9f6
machlose <150077914+machlose@users.noreply.github.com>
2025-03-28 22:20:41 +0100

    update

--- STEAM Lab/Misc/views/OffsetWrapper.swift
+++ STEAM Lab/Misc/views/OffsetWrapper.swift
@@ -11,18 +11,19 @@ struct OffsetWrapper<Label:View>: View {
     @Binding var show: Bool
     let offset: CGSize
     @State private var dynOffset: CGSize = CGSize(width: 0,height: 0)
+    @State var invertState = false
     @ViewBuilder var labelView: () -> Label
     
     var body: some View {
         labelView()
         .onAppear{
-            if !show{
+            if invertState == show{
                 dynOffset = offset
             }
         }
         .offset(dynOffset)
         .onChange(of: show){
-            if show{
+            if invertState != show{
                 withAnimation{
                     dynOffset.height = 0
                 }
--- STEAM Lab/SphereAR/PickPlanetView.swift
+++ STEAM Lab/SphereAR/PickPlanetView.swift
@@ -8,30 +8,19 @@
 import SwiftUI
 
 struct PickPlanetView: View {
-    @State private var offset: CGSize = CGSize(width: 0, height: 0)
+    @State private var offset: CGSize = CGSize(width: 0, height: -200)
     @Binding var show: Bool
     var body: some View {
-        VStack{
-            HStack{
-                Spacer()
-                Text("Kliknij w planetę aby ją wybrać")
-                Spacer()
-            }
-            .padding()
-            .background(.thinMaterial)
-            .cornerRadius(20)
-            .offset(offset)
-            .onChange(of: show){
-                if !show{
-                    withAnimation{
-                        offset.height = 0
-                    }
+        OffsetWrapper(show: $show, offset: offset, invertState: true){
+            VStack{
+                HStack{
+                    Spacer()
+                    Text("Kliknij w planetę aby ją wybrać")
+                    Spacer()
                 }
-                else{
-                    withAnimation{
-                        offset.height = -200
-                    }
-                }
+                .padding()
+                .background(.thinMaterial)
+                .cornerRadius(20)
             }
         }
     }
